990579f60a36006b054ead59f0c0135c9038a116
Edwin-Farid <117edwin@gmail.com>
2023-09-29 07:14:08 +0800

    fix: change traditional asynchronous to coroutine & lifecycle runtime

--- app/src/main/java/com/dicoding/mybackgroundthread/MainActivity.kt
+++ app/src/main/java/com/dicoding/mybackgroundthread/MainActivity.kt
@@ -6,6 +6,10 @@ import android.os.Handler
 import android.os.Looper
 import android.widget.Button
 import android.widget.TextView
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.util.concurrent.Executor
 import java.util.concurrent.Executors
 
@@ -17,30 +21,22 @@ class MainActivity : AppCompatActivity() {
         val btnStart = findViewById<Button>(R.id.btn_start)
         val tvStatus = findViewById<TextView>(R.id.tv_status)
 
-        val executor = Executors.newSingleThreadExecutor()
-        val handler = Handler(Looper.getMainLooper())
-
         btnStart.setOnClickListener {
-            executor.execute {
-                try {
-                    for (i in 0..10) {
-                        Thread.sleep(500)
-                        val percentage = i * 10
-                        handler.post {
-                            if (percentage == 100) {
-                                tvStatus.setText(R.string.task_completed)
-                            } else {
-                                tvStatus.text =
-                                    String.format(getString(R.string.compressing), percentage)
-                            }
+            lifecycleScope.launch(Dispatchers.Default) {
+                for (i in 0..10) {
+                    Thread.sleep(500)
+                    val percentage = i * 10
+                    withContext(Dispatchers.Main) {
+                        if (percentage == 100) {
+                            tvStatus.setText(R.string.task_completed)
+                        } else {
+                            tvStatus.text =
+                                String.format(getString(R.string.compressing), percentage)
                         }
-
                     }
-                } catch (e: InterruptedException) {
-                    e.printStackTrace()
+
                 }
             }
-
         }
     }
 }
